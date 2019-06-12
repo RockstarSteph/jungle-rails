15b139020f02a72784122b9be9f39f6056bcc5fa
RockstarSteph <stephaniechreifi@gmail.com>
2019-06-12 19:20:25 +0000

    finished working on branch sold out for product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
 
   def index
+    #byebug (for interactinng/checking out with your db)
     @products = Product.all.order(created_at: :desc)
   end
 
