43a0f60c38c0ad64f1ad721b50342e7e109afeea
RockstarSteph <stephaniechreifi@gmail.com>
2019-06-14 20:43:20 +0000

    added if cart is empty logic / redirect user to HP

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,8 @@
 class Admin::CategoriesController < ApplicationController
+
+   http_basic_authenticate_with name: "Jungle", password: "book", except: :index
+
+
   def index
     @categories = Category.order(id: :desc).all
 
